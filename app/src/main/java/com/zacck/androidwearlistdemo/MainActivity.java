ba0749d5cb8f5612bf9aa2d164b3d1bcf64d5b03
zacck <coderv63@gmail.com>
2016-03-01 11:01:48 +0200

    fixed up the adapter to dispay the list items and put it internal in Mainactivity

--- app/src/main/java/com/zacck/androidwearlistdemo/MainActivity.java
+++ app/src/main/java/com/zacck/androidwearlistdemo/MainActivity.java
@@ -1,8 +1,14 @@
 package com.zacck.androidwearlistdemo;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
 import android.support.wearable.view.WearableListView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainActivity extends Activity implements WearableListView.ClickListener {
 
@@ -14,15 +20,26 @@ public class MainActivity extends Activity implements WearableListView.ClickList
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
 
-            // Get the list component from the layout of the activity
-            WearableListView listView =
-            (WearableListView) findViewById(R.id.mWearList);
+            final WatchViewStub stub = (WatchViewStub)findViewById(R.id.watch_view_stub);
+            stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+                @Override
+                public void onLayoutInflated(WatchViewStub watchViewStub) {
 
-            // Assign an adapter to the list
-            listView.setAdapter(new MyListAdapter(this, elements));
+                    // Get the list component from the layout of the activity
+                    WearableListView listView =
+                            (WearableListView) findViewById(R.id.mWearList);
 
-            // Set a click listener
-            listView.setClickListener(this);
+                    MyListAdapter mAdp = new MyListAdapter(MainActivity.this, elements);
+                    // Assign an adapter to the list
+                    listView.setAdapter(mAdp);
+
+                    // Set a click listener
+                    listView.setClickListener(MainActivity.this);
+
+                }
+            });
+
+
     }
 
     // WearableListView click listener
@@ -34,5 +51,59 @@ public class MainActivity extends Activity implements WearableListView.ClickList
 
     @Override
     public void onTopEmptyRegionClick() {
+    }
+
+    private static final class MyListAdapter extends WearableListView.Adapter {
+        private String[] mDataset;
+        private final Context mContext;
+        private final LayoutInflater mInflater;
+
+        // Provide a suitable constructor (depends on the kind of dataset)
+        public MyListAdapter(Context context, String[] dataset) {
+            mContext = context;
+            mInflater = LayoutInflater.from(context);
+            mDataset = dataset;
+        }
+
+        // Provide a reference to the type of views you're using
+        public static class ItemViewHolder extends WearableListView.ViewHolder {
+            private TextView textView;
+            public ItemViewHolder(View itemView) {
+                super(itemView);
+                // find the text view within the custom item's layout
+                textView = (TextView) itemView.findViewById(R.id.name);
+            }
+        }
+
+        // Create new views for list items
+        // (invoked by the WearableListView's layout manager)
+        @Override
+        public WearableListView.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                              int viewType) {
+            // Inflate our custom layout for list items
+            return new ItemViewHolder(mInflater.inflate(R.layout.list_item, null));
+        }
+
+        // Replace the contents of a list item
+        // Instead of creating new views, the list tries to recycle existing ones
+        // (invoked by the WearableListView's layout manager)
+        @Override
+        public void onBindViewHolder(WearableListView.ViewHolder holder,
+                                     int position) {
+            // retrieve the text view
+            ItemViewHolder itemHolder = (ItemViewHolder) holder;
+            TextView view = itemHolder.textView;
+            // replace text contents
+            view.setText(mDataset[position]);
+            // replace list item's metadata
+            holder.itemView.setTag(position);
+        }
+
+        // Return the size of your dataset
+        // (invoked by the WearableListView's layout manager)
+        @Override
+        public int getItemCount() {
+            return mDataset.length;
+        }
     }
 }
