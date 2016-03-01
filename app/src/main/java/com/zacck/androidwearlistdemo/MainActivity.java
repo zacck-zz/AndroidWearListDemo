88b0ea1a719d7092e76543a5d768fc050043abea
zacck <coderv63@gmail.com>
2016-03-01 08:49:00 +0200

    Initial commit

---
+++ app/src/main/java/com/zacck/androidwearlistdemo/MainActivity.java
@@ -0,0 +1,40 @@
+package com.zacck.androidwearlistdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.support.wearable.view.WearableListView;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements WearableListView.ClickListener {
+
+    // Sample dataset for the list
+    String[] elements = { "I need a Macbook", "I need A smartphone", "I need a Smart Watch" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            // Get the list component from the layout of the activity
+            WearableListView listView =
+            (WearableListView) findViewById(R.id.mWearList);
+    
+            // Assign an adapter to the list
+            listView.setAdapter(new Adapter(this, elements));
+
+            // Set a click listener
+            listView.setClickListener(this);
+    }
+
+    // WearableListView click listener
+    @Override
+    public void onClick(WearableListView.ViewHolder v) {
+            Integer tag = (Integer) v.itemView.getTag();
+            // use this data to complete some action ...
+    }
+
+    @Override
+    public void onTopEmptyRegionClick() {
+    }
+}
---
+++ app/src/main/java/com/zacck/androidwearlistdemo/WearableListItemLayout.java
@@ -0,0 +1,65 @@
+package com.zacck.androidwearlistdemo;
+
+import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
+import android.support.wearable.view.WearableListView;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by Zacck on 3/1/2016.
+ */
+public class WearableListItemLayout extends LinearLayout
+        implements WearableListView.OnCenterProximityListener/*checks which item in the listview is in  the center */ {
+
+    private ImageView mCircle;
+    private TextView mName;
+
+    private final float mFadedTextAlpha;
+    private final int mFadedCircleColor;
+    private final int mChosenCircleColor;
+
+    public WearableListItemLayout(Context context) {
+        this(context, null);
+    }
+
+    public WearableListItemLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    //defining the style
+    public WearableListItemLayout(Context context, AttributeSet attrs,
+                                  int defStyle) {
+        super(context, attrs, defStyle);
+
+        mFadedTextAlpha = 0.8f;
+        mFadedCircleColor = getResources().getColor(R.color.grey);
+        mChosenCircleColor = getResources().getColor(R.color.blue);
+    }
+
+    // Get references to the icon and text in the item layout definition
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        // These are defined in the layout file for list items
+        // (see next section)
+        mCircle = (ImageView) findViewById(R.id.circle);
+        mName = (TextView) findViewById(R.id.name);
+    }
+
+    //when the item is in the middle
+    @Override
+    public void onCenterPosition(boolean animate) {
+        mName.setAlpha(1f);
+        ((GradientDrawable) mCircle.getDrawable()).setColor(mChosenCircleColor);
+    }
+
+    //when item is not in the middle
+    @Override
+    public void onNonCenterPosition(boolean animate) {
+        ((GradientDrawable) mCircle.getDrawable()).setColor(mFadedCircleColor);
+        mName.setAlpha(mFadedTextAlpha);
+    }
+}
